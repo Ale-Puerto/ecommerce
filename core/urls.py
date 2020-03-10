90abd8957f0615d96bf7ad2a928bb4042574a7e7
unknown <alvaroevenor.puertotellez@gmail.com>
2020-03-09 18:11:33 -0600

    Configurando rutas y nombres de espacios

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path 
+from .views import (Inicio
+                    
+                    )
+
+app_name = "core"
+
+urlpatterns = [
+    path('', Inicio.as_view(), name="inicio")
+]
