8bf3add6ae26e2da36a3fd4d18d149cce0e3b9e6
Ale_Tellez <ale_tellez@localhost.localdomain>
2020-03-21 21:09:27 -0600

    Añadiendo urls

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path 
-from .views import (Inicio
-                    
+from .views import (Inicio,
+                    ArticuloDetailView
                     )
 
 app_name = "core"
 
 urlpatterns = [
-    path('', Inicio.as_view(), name="inicio")
+    path('', Inicio.as_view(), name="inicio"),
+    path('producto/<pk>/', ArticuloDetailView.as_view(), name="producto")
 ]
