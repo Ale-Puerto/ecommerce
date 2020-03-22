fbb4087729dc9e210ea90bb4785271d216e091f0
Ale_Tellez <ale_tellez@localhost.localdomain>
2020-03-22 17:22:50 -0600

    Sobreescribiendo vista ArticuloDetailView

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import DetailView
 from django.utils.http import http_date
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Slider, Articulo
 
 # Create your views here.
@@ -20,3 +21,19 @@ class ArticuloDetailView(DetailView):
     model = Articulo
     context_object_name = 'articulo'
     template_name = "productodetail.html"
+
+    def get_object(self,):
+        try:
+            articulo = Articulo.objects.get(pk=self.kwargs['pk'])
+            mas_producto =Articulo.objects.filter(categoria=articulo.categoria)
+            context = {
+                'articulo' : articulo,
+                'mas_producto' : mas_producto
+            }
+
+        except ObjectDoesNotExist:
+            return 0
+
+        return context
+
+        
