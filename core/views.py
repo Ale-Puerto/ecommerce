ab3418ef875b80d424642210c41525aed768d1bb
Ale_Tellez <ale_tellez@localhost.localdomain>
2020-03-21 21:08:58 -0600

    Creando vista detalle producto

--- core/views.py
+++ core/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import DetailView
 from django.utils.http import http_date
-from .models import Slider
+from .models import Slider, Articulo
 
 # Create your views here.
 
 class Inicio(View):
     def get(self, request):
+        qs = Articulo.objects.reverse()[:5]
         context = {
-            "slider" : Slider.objects.all()
+            "slider" : Slider.objects.all(),
+            "nuevos" : qs
         }
         return render (self.request, "index.html", context)
+
+
+class ArticuloDetailView(DetailView):
+    model = Articulo
+    context_object_name = 'articulo'
+    template_name = "productodetail.html"
