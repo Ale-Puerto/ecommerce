5ffa1b7ecbf886dda14de1d5e6bef3cbdc319158
Ale_Tellez <ale_tellez@localhost.localdomain>
2020-03-22 18:13:53 -0600

    Modificando vista Inicio

--- core/views.py
+++ core/views.py
@@ -3,16 +3,18 @@ from django.views import View
 from django.views.generic import DetailView
 from django.utils.http import http_date
 from django.core.exceptions import ObjectDoesNotExist
-from .models import Slider, Articulo
+from .models import Slider, Articulo, Categorias
 
 # Create your views here.
 
 class Inicio(View):
     def get(self, request):
         qs = Articulo.objects.reverse()[:5]
+        get_categorias = Categorias.objects.all()
         context = {
             "slider" : Slider.objects.all(),
-            "nuevos" : qs
+            "nuevos" : qs,
+            "categoria" : get_categorias
         }
         return render (self.request, "index.html", context)
 
