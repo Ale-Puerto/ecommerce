bef7d72c062ad8f8c4776255f4c4a77a78468e04
Ale_Tellez <ale_tellez@localhost.localdomain>
2020-03-29 18:42:23 -0600

    Agregando modelos al admin

--- core/admin.py
+++ core/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from .models import (Slider,
                     Articulo,
                     ImagenRefencia,
-                    Categorias
+                    Categorias,
+                    Pedido,
+                    ArticuloPedido
                     )
 
 admin.site.register(Slider)
 admin.site.register(Articulo)
 admin.site.register(ImagenRefencia)
 admin.site.register(Categorias)
+admin.site.register(Pedido)
+admin.site.register(ArticuloPedido)
