bd3a4a11f2946917f3ed430ce20c49639baaef84
Ale_Tellez <ale_tellez@localhost.localdomain>
2020-03-29 18:39:31 -0600

    Añadir al carrito

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import DetailView
 from django.utils.http import http_date
 from django.core.exceptions import ObjectDoesNotExist
-from .models import Slider, Articulo, Categorias
-
+from .models import Slider, Articulo, Categorias, ArticuloPedido, Pedido
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
 # Create your views here.
 
 class Inicio(View):
@@ -38,4 +39,32 @@ class ArticuloDetailView(DetailView):
 
         return context
 
-        
+
+def add_to_cart(request, pk):
+    articulo = get_object_or_404(Articulo, pk=pk)
+    articulo_pedido, create = ArticuloPedido.objects.get_or_create(
+        user = request.user,
+        articulo = articulo,
+        ordenado = False
+    )
+    orden_qs = Pedido.objects.filter(user=request.user, ordenado=False)
+    if orden_qs.exists():
+        pedido = orden_qs[0]
+        if pedido.articulos_pedidos.filter(articulo__pk=articulo.pk).exists():
+            articulo_pedido.cantidad += 1
+            articulo_pedido.save()
+            #RECORDAR AÑADIR MENSAJE 
+            return redirect("core:inicio")
+        else:
+            pedido.articulos_pedidos.add(articulo_pedido)
+            pedido.save()
+            #Mensaje
+            return redirect("core:inicio")
+    else:
+        fecha_pedido = timezone.now()
+        pedido = Pedido.objects.create(
+            user=request.user,
+            fecha_pedido=fecha_pedido
+        )
+        pedido.articulos_pedidos.add(articulo_pedido)
+        return redirect("core:inicio")
