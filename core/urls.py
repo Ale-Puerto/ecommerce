4bb06e9b2a70a95017283d048d361162c09f1365
Ale_Tellez <ale_tellez@localhost.localdomain>
2020-03-29 18:39:46 -0600

    ruta añadir al carrito

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path 
 from .views import (Inicio,
-                    ArticuloDetailView
+                    ArticuloDetailView,
+                    add_to_cart
                     )
 
 app_name = "core"
 
 urlpatterns = [
     path('', Inicio.as_view(), name="inicio"),
-    path('producto/<pk>/', ArticuloDetailView.as_view(), name="producto")
+    path('producto/<pk>/', ArticuloDetailView.as_view(), name="producto"),
+    path('add_to_cart/<pk>/', add_to_cart, name="add_to_cart")
 ]
